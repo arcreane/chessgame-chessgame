ed6319543dc5735164050144c7831f1968eff8b4
Nicolas <122916001+swyxt15@users.noreply.github.com>
2026-04-18 09:50:37 +0200

    Update AIplayer

--- src/models/AIPlayer.py
+++ src/models/AIPlayer.py
@@ -9,7 +9,6 @@ class AIPlayer(Player):
         from src.models.board import taille_case
         c = random.choice("abcdefgh")
         r1, r2 = (2, 4) if self.color == 0 else (7, 5)
-        # Visualisation
         rect = pygame.Rect((ord(c)-ord('a'))*taille_case, (8-r1)*taille_case, taille_case, taille_case)
         pygame.draw.rect(screen, (0, 255, 0), rect, 5)
         pygame.display.flip()
