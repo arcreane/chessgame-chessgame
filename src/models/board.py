8662253ca017cad57be2450729294b56d33a12fd
Nicolas <122916001+swyxt15@users.noreply.github.com>
2026-04-18 08:46:50 +0200

    Correcting board superposition bug

--- src/models/board.py
+++ src/models/board.py
@@ -1,35 +1,21 @@
-from src.models.position import Position
-from src.models.pieces_types import Piece, King, Queen, Bishop, Knight, Rook, Pawn, Color_Piece
+from src.models.pieces_types import *
 
-taille_case_droite = 8
-taille_case_gauche = 10
-taille_case = taille_case_droite * taille_case_gauche
-
+taille_case = 80
 
 class BoardGame:
-    pieces_constructor = [Rook,  Knight, Bishop,  King, Queen, Bishop, Knight, Rook ]
+    pieces_constructor = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+
     def __init__(self, drawing_surface):
         self.pieces = []
-        self.grid = {}
-        self._initialize_pieces(drawing_surface)
+        self.drawing_surface = drawing_surface
+        self._initialize_pieces()
 
-    def _add_piece(self, piece):
-        self.pieces.append(piece)
-        self.grid[str(piece.position)] = piece
-
-    def _initialize_pieces(self, drawing_surface):
-        for index, contructor in enumerate(BoardGame.pieces_constructor):
-            self._add_piece(contructor(drawing_surface, 2*index// len(BoardGame.pieces_constructor), Color_Piece.WHITE))
-            self._add_piece(contructor(drawing_surface, 2 * index // len(BoardGame.pieces_constructor), Color_Piece.BLACK))
-
-
-    def getPosition(self, piece):
-        if piece in self.pieces and piece.position is not None:
-            return piece.position
-        return None
-
-    def getPiece(self, position):
-        return self.grid.get(str(position), None)
+    def _initialize_pieces(self):
+        for index, constructor in enumerate(BoardGame.pieces_constructor):
+            self.pieces.append(constructor(self.drawing_surface, index, Color_Piece.WHITE))
+            self.pieces.append(constructor(self.drawing_surface, index, Color_Piece.BLACK))
+            self.pieces.append(Pawn(self.drawing_surface, index, Color_Piece.WHITE))
+            self.pieces.append(Pawn(self.drawing_surface, index, Color_Piece.BLACK))
 
     def draw_pieces(self):
         for piece in self.pieces:
