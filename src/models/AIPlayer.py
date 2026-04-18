d7128537403ccc6bee623d26251ed3ebec219ccc
Nicolas <122916001+swyxt15@users.noreply.github.com>
2026-04-18 09:50:19 +0200

    Updating player

--- src/models/AIPlayer.py
+++ src/models/AIPlayer.py
@@ -1,11 +1,17 @@
-import random
+import random, pygame
 from src.models.player import Player
 
 class AIPlayer(Player):
-    def __init__(self, color: int):
+    def __init__(self, color):
         super().__init__("AI", color)
 
-    def askMove(self, board=None) -> str:
-        cols = "abcdefgh"
-        move = f"P{random.choice(cols)}2 {random.choice(cols)}4"
-        return move
+    def askMove(self, screen):
+        from src.models.board import taille_case
+        c = random.choice("abcdefgh")
+        r1, r2 = (2, 4) if self.color == 0 else (7, 5)
+        # Visualisation
+        rect = pygame.Rect((ord(c)-ord('a'))*taille_case, (8-r1)*taille_case, taille_case, taille_case)
+        pygame.draw.rect(screen, (0, 255, 0), rect, 5)
+        pygame.display.flip()
+        pygame.time.delay(1000)
+        return f"{c}{r1} {c}{r2}"
