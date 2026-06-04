346edfbcd4b6556574c8fcb50bf4214fe61b3557
Tom <162678147+tominatorr@users.noreply.github.com>
2026-06-04 19:12:15 +0200

    Update position.py

--- src/models/position.py
+++ src/models/position.py
@@ -8,3 +8,19 @@ class Position:
 
     def get_col(self):
         return ord(self.column) - ord('a')
+
+
+    def __eq__(self, other):
+        return self.column == other.column and self.row == other.row
+
+    @staticmethod
+    def from_string(text):
+        if len(text) != 2:
+            return None
+        col = text[0].lower()
+        if col < 'a' or col > 'h' or not text[1].isdigit():
+            return None
+        row = int(text[1])
+        if row < 1 or row > 8:
+            return None
+        return Position(col, row)
